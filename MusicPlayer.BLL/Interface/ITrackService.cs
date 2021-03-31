c9b67a30a8894f6f4ec573fcf7b59e626892ddac
Віталік <vitalik.baziuk228@gmail.com>
2021-03-31 16:46:58 +0300

    Create interfaces

---
+++ MusicPlayer.BLL/Interface/ITrackService.cs
@@ -0,0 +1,15 @@
+﻿using MusicPlayer.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.BLL.Interface
+{
+    public interface ITrackService
+    {
+        IEnumerable<TrackDTO> GetAllTracks();
+        void CreateNewTrack(TrackDTO trackDTO);
+    }
+}
