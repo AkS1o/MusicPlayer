b4a626d48306983ac459e6ee945fa293fc26fee6
Василь Колотюк <vasakolotuk02@gmail.com>
2021-03-31 17:24:21 +0300

    add Dbset

--- MusicPlayer.DAL/EF/MusicPlayerDbContext.cs
+++ MusicPlayer.DAL/EF/MusicPlayerDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MusicPlayer.DAL.Entities;
+using System;
 using System.Data.Entity;
 using System.Linq;
 
@@ -6,27 +7,19 @@ namespace MusicPlayer.DAL.EF
 {
     public class MusicPlayerDbContext : DbContext
     {
-        // Контекст настроен для использования строки подключения "MusicPlayerDbContext" из файла конфигурации  
-        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
-        // "MusicPlayer.DAL.EF.MusicPlayerDbContext" в экземпляре LocalDb. 
-        // 
-        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "MusicPlayerDbContext" 
-        // в файле конфигурации приложения.
+      
         public MusicPlayerDbContext()
             : base("name=MusicPlayerDbContext")
         {
             Database.SetInitializer(new Initializer());
         }
 
-        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
-        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
-
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public virtual DbSet<Album> Albums { get; set; }
+        public virtual DbSet<Artist> Artists { get; set; }
+        public virtual DbSet<Playlist> Playlists { get; set; }
+        public virtual DbSet<Track> Tracks { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Сategory> Сategories { get; set; }
     }
 
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
 }
