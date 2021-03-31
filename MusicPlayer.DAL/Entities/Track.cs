9398483f48b832962e9cd772897851aef00e33a0
Vlad805 <zhomyruk_ak18@nuwm.edu.ua>
2021-03-31 17:41:31 +0300

    Create WorkWithAudioFilesViewModel class and change Track class in database

--- MusicPlayer.DAL/Entities/Track.cs
+++ MusicPlayer.DAL/Entities/Track.cs
@@ -10,10 +10,10 @@ namespace MusicPlayer.DAL.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int ArtistId { get; set; }
-        public int AlbumId { get; set; }
+        public int? ArtistId { get; set; }
+        public int? AlbumId { get; set; }
         public string Duration { get; set; }
-        public int NumberOfAuditions { get; set; }
+        public int? NumberOfAuditions { get; set; }
 
         public virtual Artist Artist { get; set; }
         public virtual Album Album { get; set; }
--- MusicPlayer.UI/ViewModels/MainViewModel.cs
+++ MusicPlayer.UI/ViewModels/MainViewModel.cs
@@ -12,40 +12,13 @@ namespace MusicPlayer.UI.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        WorkWithAudoiFilesViewModel workWithAudoiFilesViewModel = new WorkWithAudoiFilesViewModel();
         public ICommand Select_dir_for_scan => select_dir_for_scan;
         private Command select_dir_for_scan;
 
         public MainViewModel()
         {
-            select_dir_for_scan = new DelegateCommand(Select_directory_for_scan_music);
+            select_dir_for_scan = new DelegateCommand(workWithAudoiFilesViewModel.Select_directory_for_scan_music);
         }
-
-        private void Skan(string sourceDir)
-        {
-            Task.Run(() =>
-            {
-                string[] picList = Directory.GetFiles(sourceDir, "*.mp3");
-                foreach (string f in picList)
-                {
-                    string fName = f.Substring(sourceDir.Length + 1);
-                    File.Copy(Path.Combine(sourceDir, fName), Path.Combine("D:\\Music_for_project", fName), true);
-                }
-            });
-        }
-        void Select_directory_for_scan_music()
-        {
-            string sourceDir = null;
-            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
-            dialog.InitialDirectory = @"C:\Users";
-            dialog.IsFolderPicker = true;
-            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
-            {
-                sourceDir = dialog.FileName;
-            }
-            if (sourceDir != null)
-                Skan(sourceDir);
-        }
-
-        
     }
 }
