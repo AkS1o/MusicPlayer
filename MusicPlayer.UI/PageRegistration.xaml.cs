523d3a7152d7f9b5402d244515a522c68e44423f
Василь Колотюк <vasakolotuk02@gmail.com>
2021-04-03 18:29:45 +0300

    add  comands

--- MusicPlayer.UI/PageRegistration.xaml.cs
+++ MusicPlayer.UI/PageRegistration.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MusicPlayer.UI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace MusicPlayer.UI
         public PageRegistration()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
